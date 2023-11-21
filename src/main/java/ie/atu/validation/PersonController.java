493d537dfdbf7cee42019d97736b32ca14bc28fc
G00407342@atu.ie <G00407342@atu.ie>
2023-11-21 16:22:53 +0000

    PERSON controller

--- src/main/java/ie/atu/validation/PersonController.java
+++ src/main/java/ie/atu/validation/PersonController.java
@@ -1,5 +1,38 @@
 package ie.atu.validation;
-
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class PersonController {
-    private  final Person
+    private final PersonService personService;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+
+    }
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<?> getPerson(@PathVariable String employeeId){
+        if(employeeId.length() > 5 || employeeId.isBlank()){
+            return ResponseEntity.badRequest().body("employee is invalid");
+
+        }
+
+        Person person = personService.getPersonEmployeeId(employeeId);
+        if(person == null){
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok(person);
+
+    }
+    @PostMapping("/createPerson")
+    public ResponseEntity<String>create(@Valid @RequestBody Person person){
+
+        personService.savePerson(person);
+        return new ResponseEntity<>("person created successfully", HttpStatus.OK);
+
+    }
+
 }
+
+
--- src/main/java/ie/atu/validation/PersonService.java
+++ src/main/java/ie/atu/validation/PersonService.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
-    public void savePerson(Person person){
+    public void savePerson(Person person) {
         System.out.println("Person saved:" + person);
 
     }
+
+    public Person getPersonEmployeeId(String EmployeeId) {
+        return new Person();
+
+    }
+
 }
+
